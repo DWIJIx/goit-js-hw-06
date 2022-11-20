7a40f6d3fc8752c5780d108f9282af75c989072f
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2022-11-20 12:48:06 +0200

    finished 2

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+let listOfIngredients = document.querySelector('ul#ingredients');
+// console.log(listOfIngredients);
+// listOfIngredients = []
+for (let i = 0; i < ingredients.length; i += 1) {
+  let aNewListItem = document.createElement('li');
+  aNewListItem.textContent = ingredients[i];
+  aNewListItem.classList.add('item');
+  listOfIngredients.append(aNewListItem)
+}
+console.log(listOfIngredients)
