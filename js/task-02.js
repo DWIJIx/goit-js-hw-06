b5faa9342713c0babf62f0e66b45b41ac5e97f11
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2022-11-21 20:00:40 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,13 +6,39 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-let listOfIngredients = document.querySelector('ul#ingredients');
-// console.log(listOfIngredients);
-// listOfIngredients = []
-for (let i = 0; i < ingredients.length; i += 1) {
-  let aNewListItem = document.createElement('li');
-  aNewListItem.textContent = ingredients[i];
-  aNewListItem.classList.add('item');
-  listOfIngredients.append(aNewListItem)
-}
-console.log(listOfIngredients)
+// let listOfIngredientsContainer  = document.querySelector('ul#ingredients');
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   let aNewListItem = document.createElement('li');
+//   aNewListItem.textContent = ingredients[i];
+//   aNewListItem.classList.add('item');
+//   listOfIngredientsContainer.append(aNewListItem)
+// }
+// console.log(listOfIngredientsContainer )
+
+
+
+// let listOfIngredientsContainer = document.querySelector('ul#ingredients');
+// let listOfIngredients = []
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const aNewListItem = document.createElement('li');
+//   aNewListItem.textContent = ingredients[i];
+//   aNewListItem.classList.add('item')
+//   listOfIngredients.push(aNewListItem);
+//   // console.log(aNewListItem)
+// }
+// console.log(listOfIngredients)
+
+// listOfIngredientsContainer.append(...listOfIngredients)
+
+
+let listOfIngredientsContainer = document.querySelector('ul#ingredients');
+
+const elements = ingredients.map(option => {
+  const aNewListItem = document.createElement('li');
+  aNewListItem.textContent = option;
+  aNewListItem.classList.add('item')
+  return aNewListItem
+})
+console.log(elements)
+listOfIngredientsContainer.append(...elements)
